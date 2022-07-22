23378f732082e1917c1e58a07201f598ca2784e9
Arthur Ryzhak <ryzhakar@gmail.com>
2022-07-22 12:52:13 +0300

    Simplify engine

--- API/engine.py
+++ API/engine.py
@@ -5,26 +5,17 @@ from aiohttp import ClientSession
 from pydantic import BaseModel
 from .endpoints import Endpoint
 
-async def post_single_request(endpoint: Endpoint, payload: BaseModel, session: Optional[ClientSession] = None) -> str:
-    if session is None:
-        async with ClientSession() as session:
-            return await post_single_request(endpoint, payload, session=session)
-    else:
-        serializable = json.loads(payload.json())
-        async with session.post(str(endpoint), json=serializable) as response:
-            raw = await response.text()
-            if response.status > 300:
-                print(raw)
-                raise ValueError(f'Request failed with status {response.status}')
-            return raw
+async def post_single_request(endpoint: Endpoint, payload: BaseModel, *, session: ClientSession):
+    serializable = json.loads(payload.json())
+    async with session.post(str(endpoint), json=serializable, raise_for_status=True) as response:
+        return response.text()
 
-async def post_multi_requests(*items: Tuple[Endpoint, BaseModel]):
-    # classes = (item[1].__class__ for item in items)
-    async with ClientSession() as session:
+async def post_multi_requests(*items: Tuple[Endpoint, BaseModel], session: ClientSession):
+    async with session:
         results = asyncio.gather(
             *[post_single_request(endpoint, payload, session=session) for endpoint, payload in items]
         )
-        return await results # type: List[str]
+        return results
 
 async def instant_fire_coroutines(*coroutines, storage: set) -> Tuple[asyncio.Task]:
     
