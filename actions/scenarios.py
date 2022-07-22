2dd755191e15981bdadceb37139f7f39f7012129
Arthur Ryzhak <ryzhakar@gmail.com>
2022-07-22 11:09:53 +0300

    Add logging to get_progression()

--- actions/scenarios.py
+++ actions/scenarios.py
@@ -40,15 +40,15 @@ async def generate_progression(full_request: PerformanceRequest) -> PerformanceR
         session_data = construct_session_data(full_request)
     req_ensure_session = get_req_ensure_session(session_data)
     ensured_session = post_single_request(*req_ensure_session)
-    logger_scenarios.debug(f"Submitted the {data_type} data from request")
+    logger_scenarios.info(f"Submitted the {data_type} data from request")
 
 
     # Alternative if statement somehow breaks the code, hence the try/except
     try:
         progression = construct_progression(performance) # type: ignore
-        logger_scenarios.debug(f"Parsed a progression from {perf_name}")
+        logger_scenarios.info(f"Parsed a progression from {perf_name}")
     except AttributeError:
-        logger_scenarios.debug(f"Can't parse a progression from {perf_name}: requesting a new one")
+        logger_scenarios.info(f"Can't parse a progression from {perf_name}: requesting a new one")
         req_prog = get_req_progression_generation(performance)
         progression_raw = await post_single_request(*req_prog)
         progression = Progression.parse_raw(progression_raw)
@@ -56,7 +56,7 @@ async def generate_progression(full_request: PerformanceRequest) -> PerformanceR
     progression_data = construct_path_data(progression)
     req_ensure_path = get_req_ensure_music(progression_data)
     ensured_path = post_single_request(*req_ensure_path)
-    logger_scenarios.debug(f"Requested and submitted a new path.")    
+    logger_scenarios.info(f"Requested and submitted a new path.")    
 
     req_voice = get_req_voices_generation(performance, progression=progression)
     voices_raw = await post_single_request(*req_voice)
@@ -66,12 +66,12 @@ async def generate_progression(full_request: PerformanceRequest) -> PerformanceR
     performance_data = construct_performance_data(progression, cheetsheet, voices)
     req_ensire_perf = get_req_ensure_music(performance_data)
     ensured_perf = post_single_request(*req_ensire_perf)
-    logger_scenarios.debug(f"Requested and submitted a new voicing.")    
+    logger_scenarios.info(f"Requested and submitted a new voicing.")    
 
     req_midihex = get_req_midihex_generation(voices)
     midihex_raw = await post_single_request(*req_midihex)
     midihex = json.loads(midihex_raw)
-    logger_scenarios.debug(f"Requested a new hex-encoded midi.")
+    logger_scenarios.info(f"Requested a new hex-encoded midi.")
 
     outcoming_performance = construct_performance(
         progression=progression,
@@ -79,12 +79,13 @@ async def generate_progression(full_request: PerformanceRequest) -> PerformanceR
         hex_blob=midihex,
         pseudo_midi=voices
     )
-    logger_scenarios.debug(f"Constructed a new performance.")
+    logger_scenarios.info(f"Constructed a new performance.")
 
     await ensured_session
     await ensured_path
     await ensured_perf
 
+    logger_scenarios.info(f"Received all responses from the server.")
     return outcoming_performance
 
 async def amend_progression(full_request: AmendmentRequest, index: int) -> PerformanceResponse:
