591f8888935f4c4cd8e353233edc3682d4841af7
Arthur Ryzhak <ryzhakar@gmail.com>
2022-09-01 22:37:17 +0300

    Modify uvicorn server launch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import uvicorn
-from microfunkhaus import APP
 
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
@@ -9,10 +8,14 @@ with open("config.json", "r") as config_file:
     KEYFILE = config["keyfile"]
     CERTFILE = config["certfile"]
 
-uvicorn.run(
-    APP,
-    host=HOST,
-    port=PORT,
-    ssl_keyfile=KEYFILE,
-    ssl_certfile=CERTFILE,
-    )
+from microfunkhaus import APP
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:APP",
+        host=HOST,
+        port=PORT,
+        debug=True,
+        reload=True,
+        ssl_keyfile=KEYFILE,
+        ssl_certfile=CERTFILE,
+        )
