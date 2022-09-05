3f41f8be7a281c24f22e5cb5d8e2da814f01a258
Arthur Ryzhak <ryzhakar@gmail.com>
2022-09-05 20:01:05 +0300

    Add a testing mode to the app object

--- microfunkhaus/app.py
+++ microfunkhaus/app.py
@@ -1,6 +1,7 @@
 import json
 import time
-from ipaddress import IPv4Address
+from ipaddress import IPv4Address, AddressValueError
+from turtle import title
 from typing import Optional
 from fastapi import (
     FastAPI,
@@ -28,123 +29,135 @@ from .actions import (
     healthcheck_dependencies,
 )
 
-
-with open("config.json", "r") as config_file:
-    config = json.load(config_file)
-    TITLE = config["title"]
-
-with open(".tokens.json", "r") as token_file:
-    TOKENS = set(json.load(token_file))
-
-async def check_token(x_token: str = Header()):
-    if x_token not in TOKENS:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-
-
-app = FastAPI(
-    title="microfunkhaus",
-    docs_url='/',
-    dependencies=[Depends(check_token)],
-    )
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-@app.middleware("http")
-async def add_process_time(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = round((time.time() - start_time) * 10**6)
-    print(f"Process time: {process_time}µs")
-    return response
-
-# Dependency
-def get_real_ip(request: Request) -> Optional[IPv4Address]:
-    client_address = request.client
-    if client_address:
-        return IPv4Address(client_address.host)
+def generate_app_with_config(testing: bool = False):
+    if testing:
+        TOKENS = {"testing",}
     else:
-        return None
+        with open(".tokens.json", "r") as token_file:
+            TOKENS = set(json.load(token_file))
 
+    with open("config.json", "r") as config_file:
+        config = json.load(config_file)
+        TITLE = config["title"]
 
-@app.on_event("startup")
-async def startup_event():
-    ok = False
-    try:
-        ok = await healthcheck_dependencies(HEALTHPOINTS)
-    finally:
-        if not ok:
-            print("Could not establish connections to all of the microservices.")
-        else:
-            print("Connections to all of the microservices are established.")
+    async def check_token(x_token: str = Header()):
+        if x_token not in TOKENS:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
-
-generation_description = """You can specify the optional key and mode (graph) parameters,
-or even supply the otherwise verbatim progression with a changed key to transpose it."""
-@app.post(
-    "/generate",
-    summary="Generates a new progression",
-    description=generation_description,
-    response_description="A generated progression",
-    response_model=PerformanceResponse,
-    status_code=status.HTTP_200_OK,
+    app = FastAPI(
+        title=TITLE,
+        docs_url='/',
+        dependencies=[Depends(check_token)],
+        )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
-async def gen_progression(
-    performance: PerformanceRequest,
-    real_ip = Depends(get_real_ip),
-    ):
-    performance.sess_id = real_ip
-    try:
-        responses = await generate_progression(performance)
-    except ClientResponseError as e:
-        raise HTTPException(status_code=e.status, detail=e.message)
-    return responses
+    @app.middleware("http")
+    async def add_process_time(request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = round((time.time() - start_time) * 10**6)
+        print(f"Process time: {process_time}µs")
+        return response
+
+    # Dependency
+    def get_real_ip(request: Request) -> Optional[IPv4Address]:
+        try:
+        # TestClient supplies an invalid
+        # adress 'testclient' in requests
+            client_address = request.client
+            if client_address:
+                return IPv4Address(client_address.host)
+            else:
+                return None
+        except AddressValueError:
+            return IPv4Address('255.255.255.255')
+
+    @app.on_event("startup")
+    async def startup_event(): # pragma: no cover
+        ok = False
+        try:
+            ok = await healthcheck_dependencies(HEALTHPOINTS)
+        finally:
+            # This awkward if statement is here
+            # so you don't need to re-raise
+            # the exception in case of failure
+            if not ok:
+                print("Could not establish connections to all of the microservices.")
+            else:
+                print("Connections to all of the microservices are established.")
 
 
-amendment_description = """It is crucially important to provide a valid performance object,
-copied verbatim from the response of the '/generate' endpoint.
-Can return a 204 'No Content' if the chord change is impossible."""
-@app.post(
-    "/amend/{index}",
-    summary="Changes a specified chord in a progression",
-    description=amendment_description,
-    response_description="An amended progression",
-    response_model=PerformanceResponse,
-    status_code=status.HTTP_200_OK,
-    )
-async def amend_performance(
-    full_request: AmendmentRequest,
-    index: int = Path(
-        ...,
-        title="Index",
-        description="The chord under this index will be substituted",
-        example=1,
-        ),
-    real_ip = Depends(get_real_ip),
-    ):
-    full_request.sess_id = real_ip
-    try:
-        responses = await amend_progression(full_request, index)
-    except ClientResponseError as e:
-        raise HTTPException(status_code=e.status, detail=e.message)
-    return responses
+    generation_description = """You can specify the optional key and mode (graph) parameters,
+    or even supply the otherwise verbatim progression with a changed key to transpose it."""
+    @app.post(
+        "/generate",
+        summary="Generates a new progression",
+        description=generation_description,
+        response_description="A generated progression",
+        response_model=PerformanceResponse,
+        status_code=status.HTTP_200_OK,
+        )
+    async def gen_progression(
+        performance: PerformanceRequest,
+        real_ip = Depends(get_real_ip),
+        ):
+        performance.sess_id = real_ip
+        try:
+            responses = await generate_progression(performance)
+        except ClientResponseError as e:
+            raise HTTPException(status_code=e.status, detail=e.message)
+        return responses
 
 
-@app.post("/label")
-async def label_progression(
-    labeling_request: LabelingRequest,
-    real_ip = Depends(get_real_ip),
-    ):
-    labeling_request.sess_id = real_ip
-    try:
-        await send_labels(labeling_request)
-    except ClientResponseError as e:
-        raise HTTPException(status_code=e.status, detail=e.message)
-    return Response(status_code=201)
+    amendment_description = """It is crucially important to provide a valid performance object,
+    copied verbatim from the response of the '/generate' endpoint."""
+    @app.post(
+        "/amend/{index}",
+        summary="Changes a specified chord in a progression",
+        description=amendment_description,
+        response_description="An amended progression",
+        response_model=PerformanceResponse,
+        status_code=status.HTTP_200_OK,
+        )
+    async def amend_performance(
+        full_request: AmendmentRequest,
+        index: int = Path(
+            ...,
+            title="Index",
+            description="The chord under this index will be substituted",
+            example=1,
+            ),
+        real_ip = Depends(get_real_ip),
+        ):
+        full_request.sess_id = real_ip
+        try:
+            responses = await amend_progression(full_request, index)
+        except ClientResponseError as e:
+            raise HTTPException(status_code=e.status, detail=e.message)
+        return responses
 
-@app.get("/healthcheck")
-async def healthcheck():
-    return Response(status_code=200)
+
+    @app.post("/label")
+    async def label_progression(
+        labeling_request: LabelingRequest,
+        real_ip = Depends(get_real_ip),
+        ):
+        labeling_request.sess_id = real_ip
+        try:
+            await send_labels(labeling_request)
+        except ClientResponseError as e:
+            raise HTTPException(status_code=e.status, detail=e.message)
+        return Response(status_code=201)
+
+    @app.get("/healthcheck")
+    async def healthcheck():
+        return Response(status_code=200)
+
+    return app
+
+app = generate_app_with_config()
