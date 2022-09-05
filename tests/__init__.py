3b3b43a494f54548aac611ff585a01ae122c647a
Arthur Ryzhak <ryzhakar@gmail.com>
2022-09-05 13:12:25 +0300

    Eliminate hardcoded fixtures
    Test adapter_functions

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+from .mocking import raw_performance
--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -1,4 +1,5 @@
 
+import devtools
 from ipaddress import IPv4Address
 from microfunkhaus import API, actions
 from hypothesis import given, strategies as st, HealthCheck, settings
@@ -6,26 +7,24 @@ from chrdiotypes.musical import ProgressionFields, ProgressionRequest, CheetShee
 from chrdiotypes.transport import PathTransport, SessionTransport, UserTransport, LabelTransport, GenericUser
 from chrdiotypes.data_enums import ChordSymbolStructures, ChordIntervalStructures, NotesInt, NodeIDs
 from .mocking import (
-    f_perf_response,
-    f_pseudomidi,
-    f_cheetsheet,
+    b_pseudomidi,
+    b_cheetsheet,
     b_progression_fields,
     b_generic_request,
-    b_labeling_request
+    b_labeling_request,
+    b_perf_response
 )
 
-from devtools import debug
 
 
 
 @given(b_progression_fields)
-def test_construct_path_data(seed_instance):
-    assert API.construct_path_data(seed_instance)
+def test_construct_path_data(progression):
+    assert API.construct_path_data(progression)
 
-@settings(suppress_health_check=[HealthCheck(9)])
-@given(progression=b_progression_fields)
-def test_construct_voicing_data(progression, f_cheetsheet, f_pseudomidi):
-    assert API.construct_voicing_data(progression, f_cheetsheet, f_pseudomidi)
+@given(progression=b_progression_fields, cheetsheet=b_cheetsheet, pseudomidi=b_pseudomidi)
+def test_construct_voicing_data(progression, cheetsheet, pseudomidi):
+    assert API.construct_voicing_data(progression, cheetsheet, pseudomidi)
 
 @given(b_generic_request)
 def test_construct_user_data(r):
@@ -36,26 +35,27 @@ def test_construct_session_data(r):
     assert API.construct_session_data(r)
 
 @given(b_labeling_request)
-def test_construct_label_data(seed_instance):
-    assert API.construct_label_data(seed_instance)
+def test_construct_label_data(r):
+    assert API.construct_label_data(r)
 
-def test_construct_progression_request(f_perf_response):
-    assert API.construct_progression_request(f_perf_response)
+@given(b_perf_response)
+def test_construct_progression_request(pr):
+    assert API.construct_progression_request(pr)
 
-@settings(suppress_health_check=[HealthCheck(9)])
-@given(progression=b_progression_fields)
-def test_construct_cheet_sheet(progression, f_perf_response):
-    assert API.construct_cheet_sheet(f_perf_response, progression=progression)
+@given(progression=b_progression_fields, pr=b_perf_response)
+def test_construct_cheet_sheet(progression, pr):
+    assert API.construct_cheet_sheet(pr, progression=progression)
 
 
-def test_construct_performance_through(f_perf_response):
-    assert API.construct_progression(f_perf_response)
+@given(b_perf_response)
+def test_construct_performance_through(pr):
+    assert API.construct_progression(pr)
 
-@settings(suppress_health_check=[HealthCheck(9)])
 @given(
     hex_blob=st.text(min_size=1),
-    progression=b_progression_fields
+    progression=b_progression_fields,
+    chsh=b_cheetsheet,
+    psdm=b_pseudomidi
     )
-def test_construct_performance(progression, f_cheetsheet, f_pseudomidi, hex_blob):
-    debug(progression)
-    assert API.construct_performance(progression=progression, cheet_sheet=f_cheetsheet, pseudo_midi=f_pseudomidi, hex_blob=hex_blob)
+def test_construct_performance(progression, chsh, psdm, hex_blob):
+    assert API.construct_performance(progression=progression, cheet_sheet=chsh, pseudo_midi=psdm, hex_blob=hex_blob)
